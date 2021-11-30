001d5b5984f9983ba409d4c57b3b3e8572e5789a
Bogdan Liusik <bodialiusik@gmail.com>
2021-12-01 00:36:44 +0200

    add UserAccessContext

---
+++ src/server apps/DiFYCore/DiFYWebApp/Modules/UserAccess/DiFY.Modules.UserAccess.Infrastructure/UserAccessContext.cs
@@ -0,0 +1,29 @@
+﻿using DiFY.Modules.UserAccess.Domain.UserRegistrations;
+using DiFY.Modules.UserAccess.Domain.Users;
+using DiFY.Modules.UserAccess.Infrastructure.Domain.UserRegistrations;
+using DiFY.Modules.UserAccess.Infrastructure.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DiFY.Modules.UserAccess.Infrastructure
+{
+    public class UserAccessContext : DbContext
+    {
+        public DbSet<UserRegistration> UserRegistrations { get; set; }
+        
+        public DbSet<User> Users { get; set; }
+
+        private readonly ILoggerFactory _loggerFactory;
+
+        public UserAccessContext(DbContextOptions options, ILoggerFactory loggerFactory) : base(options)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
+            modelBuilder.ApplyConfiguration(new UserRegistrationEntityTypeConfiguration());
+        }
+    }
+}
