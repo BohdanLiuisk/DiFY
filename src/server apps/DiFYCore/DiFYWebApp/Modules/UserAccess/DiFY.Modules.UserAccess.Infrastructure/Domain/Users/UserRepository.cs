e5d4a522a9c4ae1dcadcdfdf746abd5760e4d0c5
Bogdan Liusik <bodialiusik@gmail.com>
2021-12-01 00:41:51 +0200

    add UserRepository, UserRegistrationRepository

---
+++ src/server apps/DiFYCore/DiFYWebApp/Modules/UserAccess/DiFY.Modules.UserAccess.Infrastructure/Domain/Users/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using DiFY.Modules.UserAccess.Domain.Users;
+using DiFY.Modules.UserAccess.Domain.Users.Interfaces;
+
+namespace DiFY.Modules.UserAccess.Infrastructure.Domain.Users
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserAccessContext _userAccessContext;
+
+        public UserRepository(UserAccessContext userAccessContext)
+        {
+            _userAccessContext = userAccessContext;
+        }
+        
+        public async Task AddAsync(User user)
+        {
+            await _userAccessContext.Users.AddAsync(user);
+        }
+    }
+}
