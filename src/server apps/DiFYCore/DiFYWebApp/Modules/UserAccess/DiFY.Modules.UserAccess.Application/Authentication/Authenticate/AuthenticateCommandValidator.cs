1d28771763b2c5db48c4a723c446d3a287cb6a82
Bogdan Liusik <bodialiusik@gmail.com>
2021-12-04 03:00:17 +0200

    add AuthenticationCommandHandler

---
+++ src/server apps/DiFYCore/DiFYWebApp/Modules/UserAccess/DiFY.Modules.UserAccess.Application/Authentication/Authenticate/AuthenticateCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace DiFY.Modules.UserAccess.Application.Authentication.Authenticate
+{
+    internal class AuthenticateCommandValidator : AbstractValidator<AuthenticateCommand>
+    {
+        public AuthenticateCommandValidator()
+        {
+            RuleFor(x => x.Login).NotEmpty().WithMessage("Login cannot be empty");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Password cannot be empty");
+        }
+    }
+}
--- src/server apps/DiFYCore/DiFYWebApp/Modules/UserAccess/DiFY.Modules.UserAccess.Application/Authentication/Authenticate/AuthenticationCommandHandler.cs
+++ src/server apps/DiFYCore/DiFYWebApp/Modules/UserAccess/DiFY.Modules.UserAccess.Application/Authentication/Authenticate/AuthenticationCommandHandler.cs
@@ -1,14 +1,56 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
+using Dapper;
+using DiFY.BuildingBlocks.Application.Data;
 using DiFY.Modules.UserAccess.Application.Configuration.Commands;
+using DiFY.Modules.UserAccess.Application.Contracts;
 
 namespace DiFY.Modules.UserAccess.Application.Authentication.Authenticate
 {
-    public class AuthenticationCommandHandler : ICommandHandler<AuthenticateCommand, AuthenticationResult>
+    internal class AuthenticationCommandHandler : ICommandHandler<AuthenticateCommand, AuthenticationResult>
     {
-        public Task<AuthenticationResult> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        internal AuthenticationCommandHandler(ISqlConnectionFactory sqlConnectionFactory)
         {
-            throw new System.NotImplementedException();
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+        
+        public async Task<AuthenticationResult> Handle(AuthenticateCommand request, CancellationToken cancellationToken)
+        {
+            var connection = _sqlConnectionFactory.GetOpenConnection();
+
+            const string sql =
+                "SELECT [User].[Id], [User].[Login], [User].[Name], [User.Email], [User].[IsActive], [User].Password " +
+                "FROM [users].[Users] AS [User] " +
+                "WHERE [User].[Login] = @Login";
+
+            var user = await connection.QuerySingleOrDefaultAsync<UserDto>(sql, new  { request.Login });
+
+            if (user == null)
+            {
+                return new AuthenticationResult("Incorrect login.");
+            }
+
+            if (!user.IsActive)
+            {
+                return new AuthenticationResult("User id not active.");
+            }
+
+            if (!PasswordHashManager.VerifyHashedPassword(user.Password, request.Password))
+            {
+                return new AuthenticationResult("Incorrect password.");
+            }
+
+            user.Claims = new List<Claim>()
+            {
+                new(CustomClaimTypes.Name, user.Name),
+                new(CustomClaimTypes.Email, user.Email)
+            };
+
+            return new AuthenticationResult(user);
         }
     }
 }
