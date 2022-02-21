6f58799d8a1112711f3601f387c2638cac62b045
Bogdan Liusik <bodialiusik@gmail.com>
2022-02-21 19:11:00 +0200

    feature: implement create friendship request command

---
+++ src/server apps/DiFYCore/DiFYWebApp/Modules/Social/DiFY.Modules.Social.Application/FriendshipRequest/CreateFriendshipRequest/CreateFriendshipRequestCommand.cs
@@ -0,0 +1,23 @@
+﻿using DiFY.Modules.Social.Application.Contracts;
+using System;
+
+namespace DiFY.Modules.Social.Application.FriendshipRequests.CreateFriendshipRequest
+{
+    public class CreateFriendshipRequestCommand : CommandBase<Guid>
+    {
+        public CreateFriendshipRequestCommand(Guid requesterId, Guid addresseeId, DateTime createdDate)
+        {
+            RequesterId = requesterId;
+
+            AddresseeId = addresseeId;
+
+            CreateDate = createdDate;
+        }
+
+        public Guid RequesterId { get; }
+
+        public Guid AddresseeId { get; }
+
+        public DateTime CreateDate { get; }
+    }
+}
---
+++ src/server apps/DiFYCore/DiFYWebApp/Modules/Social/DiFY.Modules.Social.Application/FriendshipRequest/CreateFriendshipRequest/CreateFriendshipRequestCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using DiFY.Modules.Social.Application.Configuration.Commands;
+using DiFY.Modules.Social.Domain.FriendshipRequests;
+using DiFY.Modules.Social.Domain.FriendshipRequests.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiFY.Modules.Social.Application.FriendshipRequests.CreateFriendshipRequest
+{
+    internal class CreateFriendshipRequestCommandHandler : ICommandHandler<CreateFriendshipRequestCommand, Guid>
+    {
+        private readonly IFriendshipRequestRepository _friendshipRequestRepository;
+
+        private readonly IFriendshipRequestService _friendshipRequestService;
+
+        public CreateFriendshipRequestCommandHandler(
+            IFriendshipRequestRepository friendshipRequestRepository,
+            IFriendshipRequestService friendshipRequestService)
+        {
+            _friendshipRequestRepository = friendshipRequestRepository;
+
+            _friendshipRequestService = friendshipRequestService;
+        }
+
+        public async Task<Guid> Handle(CreateFriendshipRequestCommand command, CancellationToken cancellationToken)
+        {
+            var friendshipRequest = FriendshipRequest.CreateNewFriendshipRequest(
+                new RequesterId(command.RequesterId),
+                new AddresseeId(command.AddresseeId),
+                command.CreateDate,
+                _friendshipRequestService);
+
+            await _friendshipRequestRepository.AddAsync(friendshipRequest);
+
+            return friendshipRequest.Id.Value;
+        }
+    }
+}
