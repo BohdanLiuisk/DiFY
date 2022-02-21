862d8433a00813e3ac17cdcbf62a94b8d698c629
Bogdan Liusik <bodialiusik@gmail.com>
2022-02-21 20:30:02 +0200

    feature: implement confirm reject friendship request

---
+++ src/server apps/DiFYCore/DiFYWebApp/Modules/Social/DiFY.Modules.Social.Application/FriendshipRequest/ConfirmFriendshipRequest/ConfirmFriendshipRequestCommand.cs
@@ -0,0 +1,15 @@
+﻿using DiFY.Modules.Social.Application.Contracts;
+using System;
+
+namespace DiFY.Modules.Social.Application.FriendshipRequest.ConfirmFriendshipRequest
+{
+    public class ConfirmFriendshipRequestCommand : CommandBase
+    {
+        public ConfirmFriendshipRequestCommand(Guid friendshipRequestId)
+        {
+            FriendshipRequestId = friendshipRequestId;
+        }
+
+        public Guid FriendshipRequestId { get; }
+    }
+}
---
+++ src/server apps/DiFYCore/DiFYWebApp/Modules/Social/DiFY.Modules.Social.Application/FriendshipRequest/RejectFriendshipRequest/RejectFriendshipRequestCommand.cs
@@ -0,0 +1,15 @@
+﻿using DiFY.Modules.Social.Application.Contracts;
+using System;
+
+namespace DiFY.Modules.Social.Application.FriendshipRequest.RejectFriendshipRequest
+{
+    public class RejectFriendshipRequestCommand : CommandBase
+    {
+        public RejectFriendshipRequestCommand(Guid friendshipRequestId)
+        {
+            FriendshipRequestId = friendshipRequestId;
+        }
+
+        public Guid FriendshipRequestId { get; }
+    }
+}
