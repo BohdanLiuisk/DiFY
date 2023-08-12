13130ebe420246f3ec5df9ddbe57131a03bb492e
Bogdan Liusik <bodialiusik@gmail.com>
2023-08-12 18:39:22 +0300

    chore(angular): upgrade to version 16.2.0

--- frontend/src/app/core/auth/guards/auth.guard.ts
+++ frontend/src/app/core/auth/guards/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Observable, take, tap } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { selectIsLoggedIn } from '@core/auth/store/auth.selectors';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private store: Store, private router: Router) { }
 
   canActivate(): Observable<boolean> {
