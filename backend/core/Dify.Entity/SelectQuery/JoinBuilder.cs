e3c465dca3ecaeedf25979e3415900f472e509da
Bogdan Liusik <bodialiusik@gmail.com>
2024-10-07 18:06:54 +0300

    feature(query-builder): comparing to parent columns

--- backend/core/Dify.Entity/SelectQuery/JoinBuilder.cs
+++ backend/core/Dify.Entity/SelectQuery/JoinBuilder.cs
@@ -31,9 +31,57 @@ public class JoinBuilder(Query query, AliasStorage aliasStorage, JoinsStorage jo
         query.Select(queryColumns);
         var subQueryExpressions = expression.Columns.Where(c => c.Type == ExpressionType.SubQuery).ToList();
         var subQueryBuilder = new SubQueryBuilder(aliasStorage, structureManager);
-        subQueryBuilder.AppendSubQueries(query, subQueryExpressions, tableAlias);
+        subQueryBuilder.AppendSubQueries(query, subQueryExpressions, tableAlias, refEntityStructure);
         if (expression.Columns.Count != 0) {
             AppendLeftJoins(expression.Columns, tableAlias, refEntityStructure);
         }
+    }
+    
+    public ColumnPathInfo BuildColumnPathInfo(string path) {
+        var pathSegments = path.Split('.');
+        if (pathSegments.Length <= 1) {
+            var columnPath = $"{joinsStorage.StructureAlias}.{path}";
+            var columnStructure = joinsStorage.EntityStructure.FindColumn(path);
+            return new ColumnPathInfo(Path: columnPath, ColumnStructure: columnStructure);
+        }
+        var referencePath = string.Join(".", pathSegments.Take(pathSegments.Length - 1));
+        var joinMatch = joinsStorage.FindJoinPath(referencePath);
+        var columnName = pathSegments.Last();
+        if (joinMatch.Join != null && string.IsNullOrEmpty(joinMatch.LeftoverPath)) {
+            var columnStructure = joinMatch.Join.PrimaryStructure.FindColumn(columnName);
+            return new ColumnPathInfo(Path: $"{joinMatch.Join.Alias}.{columnName}", ColumnStructure: columnStructure);
+        } else {
+            var joinAlias = joinMatch.Join == null
+                ? BuildJoinPath(joinsStorage.StructureAlias, string.Empty, referencePath, joinsStorage.EntityStructure)
+                : BuildJoinPath(joinMatch.Join.Alias, joinMatch.Join.JoinPath, joinMatch.LeftoverPath, 
+                    joinMatch.Join.PrimaryStructure);
+            var columnStructure = joinsStorage.FindJoinPath(referencePath).Join!.PrimaryStructure
+                .FindColumn(columnName);
+            return new ColumnPathInfo(Path: $"{joinAlias}.{columnName}", columnStructure);
+        }
+    }
+
+    private string BuildJoinPath(string parentAlias, string existingJoinPath, string fullJoinPath, 
+        EntityStructure entityStructure) {
+        var pathSegments = fullJoinPath.Split('.');
+        var accumulatedPaths = pathSegments
+            .Select((_, index) => string.Join('.', pathSegments.Take(index + 1)))
+            .ToArray();
+        var currentAlias = parentAlias;
+        foreach (var fullColumnPath in accumulatedPaths) {
+            var columnPath = fullColumnPath.Split('.').Last();
+            var foreignKeyStructure = entityStructure.FindForeignKeyStructure(fullColumnPath);
+            var refStructure = foreignKeyStructure.ReferenceEntityStructure;
+            var completeJoinPath = string.IsNullOrEmpty(existingJoinPath) ? fullColumnPath 
+                : $"{existingJoinPath}.{fullColumnPath}";
+            var tableAlias = joinsStorage.BuildJoinAlias(columnPath, completeJoinPath, refStructure, entityStructure);
+            query.LeftJoin(
+                $"{refStructure.Name} as {tableAlias}",
+                $"{currentAlias}.{foreignKeyStructure.PrimaryColumnName}",
+                $"{tableAlias}.{refStructure.PrimaryColumn.Name}"
+            );
+            currentAlias = tableAlias;
+        }
+        return currentAlias;
     }
 }
